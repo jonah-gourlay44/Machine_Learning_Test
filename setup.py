141cbe935a8a69a829fa3cbaf36e0f9f0678b2ca
Jonah Gourlay <jonah.gourlay44@gmail.com>
2019-05-13 14:36:17 -0600

    more syntax issues

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     url='https://github.com/jonah-gourlay44/Machine_Learning_Test',
     packages=['k-NN'],
-    install_requires=['sklearn'],['pandas'],['numpy'],
+    install_requires=['numpy'],
 )
